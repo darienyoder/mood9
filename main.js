bbdc739f8d7ab6539c89fa4479c93d24c371d2e0
darienyoder <116597751+darienyoder@users.noreply.github.com>
2024-07-08 18:04:33 -0400

    Added three pages with scrolling

---
+++ main.js
@@ -0,0 +1,27 @@
+
+const max_scroll = 3
+
+setInterval(allow_scroll, 1000)
+
+window.addEventListener("wheel", event => {
+    if (can_scroll)
+    {
+        scroll_to(current_scroll + Math.sign(event.deltaY))
+    }
+    can_scroll = false
+});
+
+var can_scroll = true
+
+var current_scroll = 0;
+
+function scroll_to(scroll_id)
+{
+    current_scroll = Math.min(Math.max(scroll_id, 0), max_scroll)
+    document.getElementById(current_scroll.toString()).scrollIntoView(true);
+}
+
+function allow_scroll()
+{
+    can_scroll = true
+}
